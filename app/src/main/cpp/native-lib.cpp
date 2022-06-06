936037b8ee003636b24241fb74fd1619d5b32cf2
Starcatter <30572485+starcatter@users.noreply.github.com>
2022-06-06 15:51:50 +0200

    Add JNI multiply

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -8,3 +8,27 @@ Java_pl_edu_uksw_amap_ocl_1jni_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+extern "C"
+JNIEXPORT void JNICALL
+Java_pl_edu_uksw_amap_ocl_1jni_MainActivity_multiplyJni(JNIEnv *env, jobject thiz, jfloatArray a,
+                                                        jfloatArray b, jfloatArray result) {
+    // get Java array size
+    jsize count = env->GetArrayLength(a);
+    assert( env->GetArrayLength(a) == env->GetArrayLength(result));
+    assert( env->GetArrayLength(b) == env->GetArrayLength(result));
+
+    // get Java array data pointers
+    jfloat* a_data = env->GetFloatArrayElements( a,0);
+    jfloat* b_data = env->GetFloatArrayElements( b,0);
+    jfloat* result_data = env->GetFloatArrayElements( result,0);
+
+    // do the math!
+    for (int i = 0; i < count; ++i) {
+        result_data[i] = a_data[i] * b_data[i];
+    }
+
+    // release Java arrays
+    env->ReleaseFloatArrayElements(a, a_data, 0);
+    env->ReleaseFloatArrayElements(b, b_data, 0);
+    env->ReleaseFloatArrayElements(result, result_data, 0);
+}
--- app/src/main/java/pl/edu/uksw/amap/ocl_jni/MainActivity.java
+++ app/src/main/java/pl/edu/uksw/amap/ocl_jni/MainActivity.java
@@ -9,6 +9,8 @@ import pl.edu.uksw.amap.ocl_jni.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int MULTIPLY_SIZE = 3;
+
     // Used to load the 'ocl_jni' library on application startup.
     static {
         System.loadLibrary("ocl_jni");
@@ -26,6 +28,21 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = binding.sampleText;
         tv.setText(stringFromJNI());
+
+        // Get multiplication result from JNI
+        float[] a = new float[]{1, 2, 3};
+        float[] b = new float[]{2, 2, 2};
+        float[] result = new float[MULTIPLY_SIZE];
+        multiplyJni(a, b, result);
+
+        StringBuilder resultString = new StringBuilder();
+        for (int i = 0; i < MULTIPLY_SIZE; i++) {
+            resultString.append(String.format("%s * %s = %s\n", a[i], b[i], result[i]));
+        }
+
+        // Display JNI result
+        TextView tvJni = binding.multiplyJniResult;
+        tvJni.setText(resultString.toString());
     }
 
     /**
@@ -33,4 +50,9 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    /**
+     * Multiply arrays a and b, place result in "result" array
+     */
+    public native void multiplyJni(float[] a, float[] b, float[] result);
 }
